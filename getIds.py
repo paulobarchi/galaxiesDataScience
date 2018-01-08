c5aacf71644771a47c5dc8f1ca8e3708c6c0cf74
paulobarchi <paulobarchi@gmail.com>
2018-01-08 15:56:24 -0200

    Update getIds.py

--- getIds.py
+++ getIds.py
@@ -1,3 +1,8 @@
+# getIds.py
+# Get Ids (and/or other column values) from catalog.
+# input: catalog, column(s) and output file name.
+# output: csv file only with desired column(s).
+
 import pandas as pd
 import sys
 
@@ -5,7 +10,5 @@ import sys
 # python getIds.py <inputFile> <IdColumn> <outputFile>
 
 df = pd.read_csv(sys.argv[1])
-
 columns = [sys.argv[2]]
-
 df[columns].to_csv(sys.argv[3], index=False)
