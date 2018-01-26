1af7793fa583956e7ad7dbf04be921b02a5e19d0
paulobarchi <paulobarchi@gmail.com>
2018-01-26 13:52:42 -0200

    Update and rename get-nE-nSByMag.py to get-nE-nS-byMag.py

--- get-nE-nS-byMag.py
+++ get-nE-nS-byMag.py
@@ -1,4 +1,4 @@
-# get10E10SByMag.py
+# get-nE-nS-byMag.py
 # Sort by ascending petroMag_r and get n early- and n late-type galaxies
 # input: input catalog file name, early-type catalog file name, late-type catalog file name and final concatenated file name
 # output: catalog with n early- and n late-type galaxies by ascending petroMag_r
@@ -7,21 +7,17 @@ import pandas as pd
 import sys
 
 catalog = pd.read_csv(sys.argv[1])
-n = int(sys.argv[1])
+n = int(sys.argv[2])
 # sort by ascending magnitude
 catalog = catalog.sort_values('petroMag_r', ascending=True)
 
 # get n first E
 catalogE = catalog[(catalog['Zoo1class'] == 'E')].head(n)
-# print catalogE
-catalogE.to_csv(sys.argv[3], index=False)
 
 # get n first S
 catalogS = catalog[(catalog['Zoo1class'] == 'S')].head(n)
-# print catalogS
-catalogS.to_csv(sys.argv[4], index=False)
 
 # concatenate and save
 catalogs = [catalogE, catalogS]
 final = pd.concat(catalogs)
-final.to_csv(sys.argv[5], index=False)
+final.to_csv(sys.argv[3], index=False)
